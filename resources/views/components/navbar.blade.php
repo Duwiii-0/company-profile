5947659c0886aa2d0c0240f9b3bbf437b00d08ab
Dwii <rafifdwiarka123@gmail.com>
2025-06-19 21:35:41 +0700

    masukin .env

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-[#FF5F5F]/50 backdrop-blur-sm fixed top-3 left-3 right-3 w-full rounded-2xl text-white px-20 py-4 flex justify-between items-center">
+<nav class="bg-[#FF5F5F]/50 backdrop-blur-sm fixed min-w-[90vw] rounded-2xl text-white px-20 py-4 flex justify-between items-center">
     <div class="text-xl">Logo</div>
     <ul class="flex space-x-24  ">
         <li><a href="/" class="hover:underline">Home</a></li>
