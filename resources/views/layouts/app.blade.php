2a267b693ee78fefd0c776f4148b8ba02805e151
Dwii <rafifdwiarka123@gmail.com>
2025-06-20 22:06:00 +0700

    cek revert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,44 +1,19 @@
 <!DOCTYPE html>
-{{-- Tambahkan atribut lang untuk mendukung i18n --}}
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+<html lang="en">
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Company Profile')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{-- Tambahkan yield untuk title, dengan fallback 'Company Profile' --}}
-    <title>Adipratama Kreatif Solusi - @yield('title', 'Company Profile')</title>
-
-    {{-- Link Google Fonts untuk Poppins dan Open Sans --}}
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
-
-    {{-- Pastikan mengacu ke app.js juga jika ada JS --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js']) {{-- Atau app.jsx jika Anda menggunakannya --}}
-
-    {{-- Stack untuk CSS tambahan per halaman --}}
-    @stack('styles')
+    @vite('resources/css/app.css')
 </head>
-{{-- Terapkan font Poppins secara global sebagai default font --}}
+<body class="bg-gray-100 text-gray-900">
 
-<body class="font-poppins antialiased">
+    <x-navbar />
 
-    {{-- Sertakan Navbar dari komponen --}}
-    @include('components.navbar')
-
-    {{-- Main content area --}}
-    {{-- Hapus mt-8 karena padding-top di body akan menangani jarak dari navbar fixed --}}
-    <main>
+    <main class="container mx-auto mt-8 px-4">
         @yield('content')
     </main>
 
-    {{-- Sertakan Footer dari komponen --}}
-    @include('components.footer')
-
-    {{-- Stack untuk JS tambahan per halaman --}}
-    @stack('scripts')
+    <x-footer />
 </body>
-
 </html>
