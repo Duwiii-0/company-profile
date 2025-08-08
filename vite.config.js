429b0bb9f4f5a2aa66bc4183b6e31721970cd70b
Elmanda1 <falihelmanda@gmail.com>
2025-08-08 15:19:28 +0700

    perbaikan vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,23 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-    build: {
-        outDir: 'dist',
-        emptyOutDir: true,
-    },
-    publicDir: 'public', // semua file di public akan di-copy ke dist
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/css/portfolio.css',
-                'resources/js/app.js'
-            ],
+            input: ['resources/css/app.css', 'resources/css/portfolio.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
+        viteStaticCopy({
+            targets: [
+                { src: 'public/_redirects', dest: '' }
+            ]
+        })
     ],
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true
+    }
 });
